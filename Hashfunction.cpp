eca6114d19513f580476fb5d0722892f180e51fc
sharonrebeccawijaya-alt <sharon.rebecca.wijaya@gmail.com>
2026-03-17 13:39:47 +0800

    Use vector for dynamic array in hash function
    Replaced static array with a dynamic vector for better memory management.

--- Hashfunction.cpp
+++ Hashfunction.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <vector> 
+
 using namespace std;
 
-int main(){
+int main() {
     int size;
 
-    cout <<"Masukkan ukuran array : ";
+    cout << "Masukkan ukuran array : ";
     cin >> size;
    
-    int arr[size]={0};
+    // Menggunakan vector agar ukuran bisa dinamis dan diisi 0
+    vector<int> arr(size, 0);
 
     int input;
-    cout << "Masukkan angka yang ingin disimpan" << endl;
+    cout << "Masukkan angka yang ingin disimpan: ";
     cin >> input;
 
-    int hashValue= input % size;
-    arr[hashValue]=input;
+    int hashValue = input % size;
+    arr[hashValue] = input;
 
-    cout<<"angka "<<input<<" disimpan di index: "<<hashValue<<endl;
-return 0; 
+    cout << "Angka " << input << " disimpan di index: " << hashValue << endl;
 
+    return 0; 
 }
